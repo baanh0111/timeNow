a87a42fc9346779ef4ce75d5ba236f898edaa395
Bá Anh <baanh@Bas-MacBook-Pro.local>
2024-10-05 07:29:31 +0700

    Time Now

---
+++ hienthithoigian/hienthithoigian.java
@@ -0,0 +1,10 @@
+package hienthithoigian;
+import java.util.Date;
+
+public class hienthithoigian {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Thoi gian hien tai la: " + now);
+    }
+
+}
